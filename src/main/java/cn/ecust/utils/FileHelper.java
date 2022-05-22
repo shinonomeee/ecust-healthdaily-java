ec5a1a4607e34ef477a38452ac0c85d3de5df37b
shinonome <chris2423883715@gmail.com>
2022-05-23 00:07:22 +0800

    更新文件名

--- src/main/java/cn/ecust/utils/FileHelper.java
+++ src/main/java/cn/ecust/utils/FileHelper.java
@@ -14,7 +14,7 @@ import java.nio.file.StandardOpenOption;
  */
 public interface FileHelper {
 
-    String fileName = "fillinHistory.txt";
+    String fileName = "fillInHistory.txt";
 
     static void logWriter(String logInfo) {
         Path filePath = Paths.get(fileName);
