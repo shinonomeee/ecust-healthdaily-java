1dcd7e98ce73850991d7963b2c5b3ea2fbbddbae
shinonome <shinome@126.com>
2022-07-11 13:38:19 +0800

    微调

--- src/main/java/cn/ecust/action/HealthDailyAction.java
+++ src/main/java/cn/ecust/action/HealthDailyAction.java
@@ -3,18 +3,6 @@ package cn.ecust.action;
 
 import cn.ecust.utils.FileHelper;
 import cn.ecust.utils.FillInUtil;
-import org.openqa.selenium.By;
-import org.openqa.selenium.ElementNotInteractableException;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import javax.management.RuntimeErrorException;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import static cn.ecust.constants.Info.*;
 
 public interface HealthDailyAction extends FileHelper, FillInUtil {
 
